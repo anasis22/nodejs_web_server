cdfdff8a43e292b9806db3f1940d953d8138f98d
anas ismail <msanasismail@gmail.com>
2024-01-24 20:34:22 +0530

    j24

--- routes/subdir.js
+++ routes/subdir.js
@@ -7,7 +7,7 @@ router.get('^/$|/index(.html)?',(req,res) => {
     res.sendFile(path.join(__dirname,'..','views','subdir','index.html'))
 })
 
-router.get('^/$|/test(.html)?',(req,res) => {
+router.get('/test(.html)?',(req,res) => {
     res.sendFile(path.join(__dirname,'..','views','subdir','test.html'))
 })
 
